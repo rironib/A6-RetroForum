827090d40f537f0a1fe151c634e1218793257d2d
Razibul Islam <dev.ronib@gmail.com>
2024-03-03 15:37:36 +0600

    Post design added

---
+++ js/loading.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const discussContainer = document.getElementById("discuss-container");
+  const readContainer = document.getElementById("read-container");
+  discussContainer.style.display = readContainer.style.display = "none";
+
+  //   const loadingOverlay = document.createElement("div");
+  //   loadingOverlay.classList.add("flex", "justify-center", "items-center");
+
+  const loadingSpinner = document.createElement("div");
+  loadingSpinner.classList.add("loading", "loading-infinity", "loading-lg");
+
+  //   loadingOverlay.appendChild(loadingSpinner);
+
+  const loadingContainer = document.getElementById("discuss-section");
+  loadingContainer.appendChild(loadingSpinner);
+
+  setTimeout(function () {
+    loadingSpinner.remove();
+    discussContainer.style.display = readContainer.style.display = "block";
+  }, 2000);
+});
